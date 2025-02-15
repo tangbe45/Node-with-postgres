ca199d1be920571e486c9a7f7656de962e9d5314
tangbe lucien <lucientangbe45@gmail.com>
2025-02-15 14:02:28 +0100

    Add product update route

--- controllers/products_controller.js
+++ controllers/products_controller.js
@@ -75,3 +75,69 @@ exports.createProduct = async (req, res) => {
       .json({ severity: error.severity, name: error.name, code: error.code });
   }
 };
+
+exports.updateProduct = async (req, res) => {
+  console.log("In controller");
+  try {
+    const {
+      name,
+      description,
+      price,
+      currency,
+      quantity,
+      active,
+      category_id,
+    } = req.body;
+
+    if (!name) {
+      return res.status(422).json({ message: "Name is required" });
+    }
+
+    if (!price) {
+      return res.status(422).json({ message: "Price is required" });
+    }
+
+    if (!category_id) {
+      return res.status(422).json({ message: "Category id is required" });
+    } else {
+      const existsResult = await database.pool.query({
+        text: `SELECT EXISTS (SELECT * FROM categories WHERE id = $1)`,
+        values: [category_id],
+      });
+
+      if (!existsResult.rows[0].exists) {
+        return res.status(422).json({ message: "Category id not found" });
+      }
+    }
+    console.log("After validation");
+    const result = await database.pool.query({
+      text: `
+        UPDATE products 
+        SET name = $1, description = $2, price = $3, currency = $4, quantity = $5, active = $6, category_id = $7
+        WHERE id = $8 RETURNING *`,
+      values: [
+        name,
+        description ? description : null,
+        price,
+        currency ? currency : "CFA",
+        quantity ? quantity : 0,
+        "active" in req.body ? active : "true",
+        category_id,
+        req.params.id,
+      ],
+    });
+    console.log("After update");
+    if (!result.rowCount) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    return res.status(200).json(result.rows[0]);
+  } catch (error) {
+    return res.status(500).json({
+      severity: error.severity,
+      name: error.name,
+      code: error.code,
+      message: error.message,
+    });
+  }
+};
--- routes/products_routes.js
+++ routes/products_routes.js
@@ -1,11 +1,13 @@
 const {
   getAllProducts,
   createProduct,
+  updateProduct,
 } = require("../controllers/products_controller");
 
 const router = require("express").Router();
 
 router.get("/", getAllProducts);
 router.post("/", createProduct);
+router.put("/:id", updateProduct);
 
 module.exports = router;
