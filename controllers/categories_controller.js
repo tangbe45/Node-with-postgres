92c729d3f45a30488a3c32cde7d027425d2404a2
tangbe lucien <lucientangbe45@gmail.com>
2025-02-15 04:38:03 +0100

    Add create category route

--- controllers/categories_controller.js
+++ controllers/categories_controller.js
@@ -10,3 +10,37 @@ exports.getAllCategories = async (req, res) => {
       .json({ severity: error.severity, name: error.name, code: error.code });
   }
 };
+
+exports.createCategory = async (req, res) => {
+  try {
+    const { name } = req.body;
+    if (!name) {
+      return res.status(422).json({ message: "Name is required" });
+    }
+
+    const existsResult = await database.pool.query({
+      text: "SELECT EXISTS( SELECT * FROM categories WHERE name = $1)",
+      values: [name],
+    });
+
+    if (existsResult.rows[0].exists) {
+      return res
+        .status(409)
+        .json({ message: `Category ${name} already exist` });
+    }
+
+    const result = await database.pool.query({
+      text: `INSERT INTO categories (name) VALUES ($1) RETURNING *`,
+      values: [name],
+    });
+    return res.status(201).json(result.rows[0]);
+  } catch (error) {
+    return res.status(500).json({
+      severity: error.severity,
+      name: error.name,
+      code: error.code,
+      message: error.message,
+      detail: error.detail,
+    });
+  }
+};
--- routes/catagories_routes.js
+++ routes/catagories_routes.js
@@ -1,7 +1,11 @@
-const { getAllCategories } = require("../controllers/categories_controller");
+const {
+  getAllCategories,
+  createCategory,
+} = require("../controllers/categories_controller");
 
 const router = require("express").Router();
 
 router.get("/", getAllCategories);
+router.post("/", createCategory);
 
 module.exports = router;
